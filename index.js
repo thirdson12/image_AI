11f2cf18a70388ab70ff60724ad60bcf3d4ddc62
bertu <74742652+thirdson12@users.noreply.github.com>
2023-02-02 03:29:14 +0200

    frontend
    api called in frontend

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const path = require('path');
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -7,6 +8,9 @@ const app = express();
 // data alma 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
+
+//static html staff
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use(`/openai`, require(`./routes/openaiRoutes`))
